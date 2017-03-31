f0a1290a91b0762a8244fa55798bde2f3d82539e
SXLFLL <2636524473@qq.com>
2017-03-31 22:22:25 +0800

    添加
    创建

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(e) {
+	time=setInterval("$.change()",5000);
+
+	t=1;
+	$("#t-"+t).fadeIn();
+	$.change=function(){
+		t++;
+		if (t<=$(".title").length) {
+		$(".title").slideUp(1500).fadeOut();
+		$("#t-"+t).slideDown(1500).fadeIn();
+
+		}else{
+			window.location="content.html";
+		}
+	}
+});
